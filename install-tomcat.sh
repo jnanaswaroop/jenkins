a5c299a4537a7dabe45305d5f8ec135c6d436545
jnanaswaroop <118235880+jnanaswaroop@users.noreply.github.com>
2023-04-10 23:09:56 +0530

    Update install-tomcat.sh

--- install-tomcat.sh
+++ install-tomcat.sh
@@ -5,25 +5,25 @@ apt upgrade -y
 apt install default-jdk -y
 java -version
 apt install maven -y
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.7/bin/apache-tomcat-10.1.7.tar.gz
-sudo tar -xvzf apache-tomcat-10.1.7.tar.gz
-sudo rm -rf apache-tomcat-10.1.7.tar.gz
-sudo mv apache-tomcat-10.1.7 /opt/tomcat
-sudo sed -i s/8080/9090/g /opt/tomcat/conf/server.xml
-sudo sh /opt/tomcat/bin/shutdown.sh
+wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.7/bin/apache-tomcat-10.1.7.tar.gz
+tar -xvzf apache-tomcat-10.1.7.tar.gz
+rm -rf apache-tomcat-10.1.7.tar.gz
+mv apache-tomcat-10.1.7 /opt/tomcat
+sed -i s/8080/9090/g /opt/tomcat/conf/server.xml
+sh /opt/tomcat/bin/shutdown.sh
 sleep 2
-sudo sh /opt/tomcat/bin/startup.sh
-sudo rm -rf conf-and-webapps-file
-sudo git clone https://github.com/syedwaliuddin/conf-and-webapps-file.git
-sudo rm -rf /opt/tomcat/conf/tomcat-users.xml
-sudo cp conf-and-webapps-file/tomcat-users.xml /opt/tomcat/conf/
-sudo sh /opt/tomcat/bin/shutdown.sh
+sh /opt/tomcat/bin/startup.sh
+rm -rf conf-and-webapps-file
+git clone https://github.com/syedwaliuddin/conf-and-webapps-file.git
+rm -rf /opt/tomcat/conf/tomcat-users.xml
+cp conf-and-webapps-file/tomcat-users.xml /opt/tomcat/conf/
+sh /opt/tomcat/bin/shutdown.sh
 sleep 2
-sudo sh /opt/tomcat/bin/startup.sh
-sudo rm -rf /opt/tomcat/webapps/manager/META-INF/context.xml
-sudo cp conf-and-webapps-file/context.xml /opt/tomcat/webapps/manager/META-INF/
-sudo rm -rf /opt/tomcat/webapps/host-manager/META-INF/context.xml
-sudo cp conf-and-webapps-file/contexthm.xml /opt/tomcat/webapps/host-manager/META-INF/
-sudo sh /opt/tomcat/bin/shutdown.sh
+sh /opt/tomcat/bin/startup.sh
+rm -rf /opt/tomcat/webapps/manager/META-INF/context.xml
+cp conf-and-webapps-file/context.xml /opt/tomcat/webapps/manager/META-INF/
+rm -rf /opt/tomcat/webapps/host-manager/META-INF/context.xml
+cp conf-and-webapps-file/contexthm.xml /opt/tomcat/webapps/host-manager/META-INF/
+sh /opt/tomcat/bin/shutdown.sh
 sleep 2
-sudo sh /opt/tomcat/bin/startup.sh
+sh /opt/tomcat/bin/startup.sh
