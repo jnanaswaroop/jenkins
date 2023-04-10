44c20a9e7ed6309b3fab0a863c44a7a0ac604963
jnanaswaroop <118235880+jnanaswaroop@users.noreply.github.com>
2023-04-10 21:30:56 +0530

    Create install-maven-tomcat.sh

--- install-maven-tomcat.sh
+++ install-maven-tomcat.sh
@@ -1,13 +1,14 @@
 #! /bin/bash
+# On root run the following command
 sudo apt update -y
 sudo apt upgrade -y
 sudo apt install default-jdk -y
 sudo java -version
 sudo apt install maven -y
-sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
-sudo tar -xvzf apache-tomcat-10.0.27.tar.gz
-sudo rm -rf apache-tomcat-10.0.27.tar.gz
-sudo mv apache-tomcat-10.0.27 /opt/tomcat
+sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.1.7/bin/apache-tomcat-10.1.7.tar.gz
+sudo tar -xvzf apache-tomcat-10.1.7.tar.gz
+sudo rm -rf apache-tomcat-10.1.7.tar.gz
+sudo mv apache-tomcat-10.1.7 /opt/tomcat
 sudo sed -i s/8080/9090/g /opt/tomcat/conf/server.xml
 sudo sh /opt/tomcat/bin/shutdown.sh
 sleep 2
