6ce9d3bb3491a28c5d2e3c4e8e6c1bd6bd77c9b0
jnanaswaroop <118235880+jnanaswaroop@users.noreply.github.com>
2023-04-10 20:51:00 +0530

    Update and rename install-tomcat.sh to install-maven-tomcat.sh

--- install-maven-tomcat.sh
+++ install-maven-tomcat.sh
@@ -3,6 +3,7 @@ sudo apt update -y
 sudo apt upgrade -y
 sudo apt install default-jdk -y
 sudo java -version
+sudo apt install maven -y
 sudo wget https://dlcdn.apache.org/tomcat/tomcat-10/v10.0.27/bin/apache-tomcat-10.0.27.tar.gz
 sudo tar -xvzf apache-tomcat-10.0.27.tar.gz
 sudo rm -rf apache-tomcat-10.0.27.tar.gz
